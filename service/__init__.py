e9df46228722f6db6ca2d25f1f4666789c9d7f69
kritika1602 <moroliakritika.18@gmail.com>
2025-05-06 03:45:05 -0400

    added cors headers

--- service/__init__.py
+++ service/__init__.py
@@ -13,9 +13,11 @@ from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
+CORS(app)
 talisman = Talisman(app)
 CORS(app)
 app.config.from_object(config)
+
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -102,12 +102,11 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
+        # Check for the CORS header 
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
 
     def test_create_account(self):
